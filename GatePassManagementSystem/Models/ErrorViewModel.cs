6e611467d6b069300e7fb0949d4a7ea54f93806a
Sandesh Piumantha <sandeshpiumantha52@gmail.com>
2026-04-16 09:48:08 +0530

    Initializing project

---
+++ GatePassManagementSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GatePassManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
