a8d7e926ce0ce4ec53f8a83a402abd3db165be10
Sandesh Piumantha <sandeshpiumantha52@gmail.com>
2026-05-24 14:10:27 +0530

    pending request page creating | status: not complete

---
+++ GatePassManagementSystem/Controllers/PendingRequestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GatePassManagementSystem.Controllers
+{
+    public class PendingRequestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
