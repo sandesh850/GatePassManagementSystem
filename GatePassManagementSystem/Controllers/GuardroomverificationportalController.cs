c70ab933b30fc0e72f6f5aeea40eb1cd4fd24c99
Sandesh Piumantha <sandeshpiumantha52@gmail.com>
2026-06-27 15:27:13 +0530

    Initializing create Guardroom verification page

---
+++ GatePassManagementSystem/Controllers/GuardroomverificationportalController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GatePassManagementSystem.Controllers
+{
+    public class GuardroomverificationportalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
