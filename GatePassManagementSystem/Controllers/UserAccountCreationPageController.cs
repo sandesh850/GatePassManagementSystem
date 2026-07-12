3f0a9c34a02d20d1cef5642a181f67a5a8e10944
Sandesh Piumantha <sandeshpiumantha52@gmail.com>
2026-07-12 12:28:31 +0530

    Backend development in user account creation page

--- GatePassManagementSystem/Controllers/UserAccountCreationPageController.cs
+++ GatePassManagementSystem/Controllers/UserAccountCreationPageController.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GatePassManagementSystem.Data;
+using GatePassManagementSystem.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GatePassManagementSystem.Controllers
 {
     public class UserAccountCreationPageController : Controller
     {
+        // dependency injection
+        private readonly AppDb _AppDb;
+
+        public UserAccountCreationPageController(AppDb _appDb)
+        {
+            _AppDb = _appDb;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            return View(new UserAccountCreationViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserAccountCreationViewModel Model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(Model);
+            }
+            else
+            {
+                return View();
+            }
+           
         }
     }
 }
---
+++ GatePassManagementSystem/Models/ViewModels/UserAccountCreationViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GatePassManagementSystem.Models.ViewModels
+{
+    public class UserAccountCreationViewModel
+    {
+        [Required(ErrorMessage ="Please Select a Employee Photo")]
+        public IFormFile? EmployeePhoto { get; set;  }
+
+        [Required(ErrorMessage = "Please Enter the Employee Name")]
+        public string? EmployeeName { get; set; }
+
+        [Required(ErrorMessage = "Please Enter the Username(EPF)")]
+        public int? Username { get; set; }
+
+        [Required(ErrorMessage = "Please Enter the Password")]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "Please Select the User Role")]
+        public string? UserRole { get; set; }
+
+        [Required(ErrorMessage = "Please Enter the Reporting Supervisor")]
+        public string? ReportingSupervisor { get; set; }
+
+        [Required(ErrorMessage = "Please Select the Reporting Manager")]
+        public string? ReportingManager { get; set; }
+
+        [Required(ErrorMessage = "Please Select the Department")]
+        public string? DPM { get; set; }
+
+        [Required(ErrorMessage = "Please Select the User Role Type")]
+        public string? UserRoleType { get; set; }
+
+    }
+}
