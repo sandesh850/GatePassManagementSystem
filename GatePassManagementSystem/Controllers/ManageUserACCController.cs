2d786c8aa868becfc97769b1eac602b69576d28e
Sandesh Piumantha <sandeshpiumantha52@gmail.com>
2026-07-24 10:21:58 +0530

    modification in user acc manage page

--- GatePassManagementSystem/Controllers/ManageUserACCController.cs
+++ GatePassManagementSystem/Controllers/ManageUserACCController.cs
@@ -1,6 +1,9 @@
 ﻿using GatePassManagementSystem.Data;
+using GatePassManagementSystem.Models.EntityModels;
 using GatePassManagementSystem.Models.ViewModels;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GatePassManagementSystem.Controllers
 {
@@ -20,20 +23,43 @@ namespace GatePassManagementSystem.Controllers
             return View(new ManageUserACCViewModel());
         }
 
+        public TblUserAccountDetails? DST_TblUserAccount_details {  get; set; }
+
         [HttpPost]
         public async Task<IActionResult> Index(ManageUserACCViewModel Model)
         {
-         
-            if (!ModelState.IsValid)
+            //DST_TblUserAccount_details = await _AppDb.TblUserAccountDetails
+            //        .FirstOrDefaultAsync(x => x.Username == Convert.ToInt16(Model.SearchingEPF));
+
+            var result = await _AppDb.TblUserAccountDetails
+                .FirstOrDefaultAsync(x => x.Username == Convert.ToInt16(Model.SearchingEPF));
+
+            if (result == null)
             {
-                return View(Model);
+                Model.EmployeeName = "No Record Found";
             }
             else
             {
-                return RedirectToAction("Index", "ManageUserACC");
+                Model.EmployeeName = result.EmployeeName;
             }
 
-            
+            return View(Model);
+
+            //return RedirectToAction("Index", "ManageUserACC");
+
+            //if (!ModelState.IsValid)
+            //{
+            //    return View(Model);
+            //}
+            //else
+            //{
+            //    DST_TblUserAccount_details = await _AppDb.TblUserAccountDetails.FindAsync(Model.SearchingEPF);
+            //    Model.EmployeeName = DST_TblUserAccount_details.EmployeeName;
+
+            //    return RedirectToAction("Index", "ManageUserACC");
+            //}
+
+
         }
     }
 }
--- GatePassManagementSystem/Models/ViewModels/ManageUserACCViewModel.cs
+++ GatePassManagementSystem/Models/ViewModels/ManageUserACCViewModel.cs
@@ -4,32 +4,34 @@ namespace GatePassManagementSystem.Models.ViewModels
 {
     public class ManageUserACCViewModel
     {
-        [Required(ErrorMessage = "Please Select a Employee Photo")]
+        [Required(ErrorMessage = "Please Enter a EPF No")]
+        public string? SearchingEPF { get; set; }
+
+        
         public IFormFile? EmployeePhoto { get; set; }
 
-        [Required(ErrorMessage = "Please Enter the Employee Name")]
+       
         public string? EmployeeName { get; set; }
 
-        [Required]
-        [Range(1, int.MaxValue, ErrorMessage = "Please enter the Username (EPF)")]
+       
         public int Username { get; set; }
 
-        [Required(ErrorMessage = "Please Enter the Password")]
+        
         public string? Password { get; set; }
 
-        [Required(ErrorMessage = "Please Select the User Role")]
+       
         public string? UserRole { get; set; }
 
-        [Required(ErrorMessage = "Please Enter the Reporting Supervisor")]
+        
         public string? ReportingSupervisor { get; set; }
 
-        [Required(ErrorMessage = "Please Select the Reporting Manager")]
+       
         public string? ReportingManager { get; set; }
 
-        [Required(ErrorMessage = "Please Select the Department")]
+        
         public string? DPM { get; set; }
 
-        [Required(ErrorMessage = "Please Select the User Role Type")]
+        
         public string? UserRoleType { get; set; }
     }
 }
